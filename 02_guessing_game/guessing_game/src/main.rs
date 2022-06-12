792364acb04ad4e04fdc59ee29adac19a496d969
Gustavo <jgustavoas@gmail.com>
2022-06-12 12:03:53 -0300

    Using "rand" and generating a secret ramdom number

--- 02_guessing_game/guessing_game/src/main.rs
+++ 02_guessing_game/guessing_game/src/main.rs
@@ -1,7 +1,15 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    let secre_number = rand::thread_rng().gen_range(1..101);
+    /* gen_range(start..end) is inclusive on the lower bound but exclusive on the upper bound,
+    so we need to specify 1..101 to request a number between 1 and 100.
+    Alternatively, we could pass the range 1..=100, which is equivalent. */
+
+    println!("The secret number is {}", secre_number);
 
     println!("Please input your guess!");
 
